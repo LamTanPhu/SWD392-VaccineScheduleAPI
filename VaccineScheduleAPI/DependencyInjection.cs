ba400d2e17123104c580cdc7433fce7036376c69
Trần Minh Trí <tritmse173635@fpt.edu.vn>
2025-02-23 15:01:27 +0700

    push fix Payment problem, can null at CenterID, fix context

--- VaccineScheduleAPI/DependencyInjection.cs
+++ VaccineScheduleAPI/DependencyInjection.cs
@@ -12,27 +12,27 @@ namespace VaccineScheduleAPI
     {
         public static void AddConfig(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDatabase(configuration);  // Registers MySQL Database
+            //services.AddDatabase(configuration);  // Registers MySQL Database
             services.AddInfrastructure();  // Registers Repositories from Service Project
             services.AddServices();        // Registers Services (API-Specific)
         }
 
-        private static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
-        {
-            // Registering DbContext with Pomelo MySQL provider and enabling lazy loading
-            services.AddDbContext<DatabaseContext>(options =>
-            {
-                var connectionString = configuration.GetConnectionString("DefaultConnection");
+        //private static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        //{
+        //    // Registering DbContext with Pomelo MySQL provider and enabling lazy loading
+        //    services.AddDbContext<DatabaseContext>(options =>
+        //    {
+        //        var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-                if (string.IsNullOrEmpty(connectionString))
-                {
-                    throw new InvalidOperationException("The connection string 'DefaultConnection' is missing.");
-                }
+        //        if (string.IsNullOrEmpty(connectionString))
+        //        {
+        //            throw new InvalidOperationException("The connection string 'DefaultConnection' is missing.");
+        //        }
 
-                options.UseLazyLoadingProxies() // Enable Lazy Loading
-                       .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-            });
-        }
+        //        options.UseLazyLoadingProxies() // Enable Lazy Loading
+        //               .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        //    });
+        //}
 
         private static void AddServices(this IServiceCollection services)
         {
