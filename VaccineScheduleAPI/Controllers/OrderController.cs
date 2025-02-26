79780d37a9e174b1fe37f08971efd27ca7725bbd
Lâm Tấn Phú <phultse182607@fpt.edu.vn>
2025-02-26 20:47:51 +0700

    Final setup

---
+++ VaccineScheduleAPI/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VaccineScheduleAPI.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
