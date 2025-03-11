867ac66392a0f6b789fb46567da09b53b8280184
AshangiCode <163718692+AshangiCode@users.noreply.github.com>
2025-03-11 10:46:59 +0700

    CRUD-Order-Schedule

---
+++ ModelViews/Requests/Schedule/UpdateScheduleRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViews.Requests.Schedule
+{
+    public class UpdateScheduleRequestDTO
+    {
+        public int DoseNumber { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public DateTime? ActualDate { get; set; }
+        public string? AdministeredBy { get; set; }
+        public int Status { get; set; } // 0: hủy lịch, 1: đặt lịch nhưng chưa đến, 2: đến rồi
+    }
+}
--- Repositories/Repository/Orders/OrderRepository.cs
+++ Repositories/Repository/Orders/OrderRepository.cs
@@ -6,11 +6,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repositories.Repository.Orders
 {
     public class OrderRepository : GenericRepository<Order>, IOrderRepository
     {
         public OrderRepository(DatabaseContext context) : base(context) { }
+
+
     }
 }
