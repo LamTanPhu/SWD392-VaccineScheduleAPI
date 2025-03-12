67cc0be7cb36efdd5bafdd02dbdf5300876071a7
Trần Minh Trí <tritmse173635@fpt.edu.vn>
2025-03-12 20:48:22 +0700

    Add Address in ChildrenProfile

---
+++ ModelViews/Requests/ChildrenProfile/ChildrenProfileRequestDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViews.Requests.ChildrenProfile
+{
+    public class ChildrenProfileRequestDTO
+    {
+        [Required]
+        public string AccountId { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Status { get; set; }
+        public string Address { set; get; }
+    }
+}
--- VaccineScheduleAPI/Controllers/ChildrenProfileController.cs
+++ VaccineScheduleAPI/Controllers/ChildrenProfileController.cs
@@ -1,12 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IServices.Interfaces.Accounts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ModelViews.Requests.ChildrenProfile;
+using ModelViews.Responses.ChildrenProfile;
 
 namespace VaccineScheduleAPI.Controllers
 {
-    public class ChildrenProfileController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChildrenProfileController : ControllerBase
     {
-        public IActionResult Index()
+
+        private readonly IChildrenProfileService _service;
+
+        public ChildrenProfileController(IChildrenProfileService service)
         {
-            return View();
+            _service = service;
+        }
+
+        [Authorize(Roles = "Admin, Parent")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ChildrenProfileResponseDTO>>> GetAll()
+        {
+            return Ok(await _service.GetAllProfilesAsync());
+        }
+
+        [Authorize(Roles = "Admin, Parent")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ChildrenProfileResponseDTO>> GetById(string id)
+        {
+            var profile = await _service.GetProfileByIdAsync(id);
+            if (profile == null) return NotFound();
+            return Ok(profile);
+        }
+
+        [Authorize(Roles = "Parent")]
+        [HttpPost]
+        public async Task<ActionResult> Create(ChildrenProfileRequestDTO profileDto)
+        {
+            await _service.AddProfileAsync(profileDto);
+            return CreatedAtAction(nameof(GetById), new { id = profileDto.FullName }, profileDto);
+        }
+
+        [Authorize(Roles = "Parent")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(string id, ChildrenProfileRequestDTO profileDto)
+        {
+            await _service.UpdateProfileAsync(id, profileDto);
+            return NoContent();
+        }
+
+        [Authorize(Roles = "Parent")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            await _service.DeleteProfileAsync(id);
+            return NoContent();
         }
     }
 }
+
