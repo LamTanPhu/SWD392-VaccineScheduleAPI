2d31fa18a3f94bdbadac3052a5a3897fb22a4a65
Lâm Tấn Phú <phultse182607@fpt.edu.vn>
2025-03-13 17:03:22 +0700

    Misc changes

--- VaccineScheduleAPI/Controllers/PaymentController.cs
+++ VaccineScheduleAPI/Controllers/PaymentController.cs
@@ -68,8 +68,6 @@ namespace VaccineScheduleAPI.Controllers
 
             return Ok(response);
         }
-     
-        
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(string id, [FromBody] PaymentDetailsRequestDTO request)
