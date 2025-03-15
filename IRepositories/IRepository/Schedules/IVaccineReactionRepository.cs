f5701f40382be32daafe66d0ba35f8b2cddb6920
Lâm Tấn Phú <phultse182607@fpt.edu.vn>
2025-03-15 22:38:26 +0700

    Reject merge

--- IRepositories/IRepository/Schedules/IVaccineReactionRepository.cs
+++ IRepositories/IRepository/Schedules/IVaccineReactionRepository.cs
@@ -20,3 +20,4 @@ namespace IRepositories.IRepository.Schedules
         }
     }
 
+}
--- Repositories/Repository/Schedules/VaccineHistoryRepository.cs
+++ Repositories/Repository/Schedules/VaccineHistoryRepository.cs
@@ -1,36 +1,32 @@
-
-﻿using Core;
+﻿using IRepositories.Entity.Schedules;
+using IRepositories.IRepository.Schedules;
 using Microsoft.EntityFrameworkCore;
-using IRepositories.IRepository;
 using Repositories.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using IRepositories.Entity.Schedules;
-using IRepositories.IRepository.Schedules;
 
 namespace Repositories.Repository.Schedules
-{    
-    
-        public class VaccineHistoryRepository : GenericRepository<VaccineHistory>, IVaccineHistoryRepository
+{
+    public class VaccineHistoryRepository : GenericRepository<VaccineHistory>, IVaccineHistoryRepository
+    {
+        public VaccineHistoryRepository(DatabaseContext context) : base(context) { }
+
+        public async Task<IEnumerable<VaccineHistory>> GetByUserIdAsync(string userId)
         {
-            public VaccineHistoryRepository(DatabaseContext context) : base(context) { }
+            return await _dbSet.Where(vh => vh.AccountId == userId).ToListAsync();
+        }
 
-            public async Task<IEnumerable<VaccineHistory>> GetByUserIdAsync(string userId)
-            {
-                return await _dbSet.Where(vh => vh.AccountId == userId).ToListAsync();
-            }
+        public async Task<IEnumerable<VaccineHistory>> SearchByDateAsync(string userId, DateTime date)
+        {
+            return await _dbSet.Where(vh => vh.AccountId == userId && vh.AdministeredDate.Date == date.Date).ToListAsync();
+        }
 
-            public async Task<IEnumerable<VaccineHistory>> SearchByDateAsync(string userId, DateTime date)
-            {
-                return await _dbSet.Where(vh => vh.AccountId == userId && vh.AdministeredDate.Date == date.Date).ToListAsync();
-            }
-
-            public async Task<IEnumerable<VaccineHistory>> SearchByCenterIdAsync(string centerId)
-            {
-                return await _dbSet.Where(vh => vh.CenterId == centerId).ToListAsync();
-            }
+        public async Task<IEnumerable<VaccineHistory>> SearchByCenterIdAsync(string centerId)
+        {
+            return await _dbSet.Where(vh => vh.CenterId == centerId).ToListAsync();
         }
     }
-
-
+}
