1e177e0d11f38d6f09d889594f35e77506a2ee96
Trần Minh Trí <tritmse173635@fpt.edu.vn>
2025-03-16 18:34:41 +0700

    bonus RUD for VaccineHistory if necessary

---
+++ ModelViews/Requests/VaccineHistory/UpdateVaccineHistoryRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViews.Requests.VaccineHistory
+{
+    public class UpdateVaccineHistoryRequestDTO
+    {
+        public DateTime AdministeredDate { get; set; }
+        public string AdministeredBy { get; set; }
+        public bool DocumentationProvided { get; set; }
+        public string Notes { get; set; }
+        public int VerifiedStatus { get; set; }
+        public int VaccinedStatus { get; set; }
+        public int DosedNumber { get; set; }
+    }
+}
